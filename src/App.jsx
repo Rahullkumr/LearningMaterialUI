cbee046cff9ce39f5c83a4d3d1ebc8b21b6f749c
Rahul Kumar <kumarrahul771996@gmail.com>
2025-02-15 22:03:55 +0530

    replace Button with HelloButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import "./App.css";
+import HelloButton from "./components/HelloButton";
 
 function App() {
   return (
     <>
       <div style={{ display: "flex", alignItems: "center", justifyContent: 'center', height: '90vh'}}>
-        <Button variant="outlined">Hello world</Button>
+        <HelloButton/>
       </div>
     </>
   );
---
+++ src/components/HelloButton.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material"
+
+
+let HelloButton = ()=>{
+    return (
+        <>
+        <Button variant="outlined">Hello Button</Button>
+        </>
+    )
+}
+export default HelloButton
